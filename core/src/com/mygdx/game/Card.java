3cd0a3d9bf2e1087585b54cd70c7dea7dc60006c
Kenneth <soreasan@gmail.com>
2015-09-16 19:04:09 -0600

    Created the Card class
    I still can't get Eclipse to work so I created the Card class using
    Notepad.  Unfortunately if it's buggy I don't have a way to test it
    right now.  =(
    However It's a pretty simple class so I'm hoping it will work.  For both
    of the data members I created enumerated types.  In order to access the
    data members as Strings I added methods that allow you to grab the
    card's Face and Suit as Strings.  The card's data members are only
    protected so if you did want to just access it from other classes within
    the same package it should be possible.

--- core/src/com/mygdx/game/Card.java
+++ core/src/com/mygdx/game/Card.java
@@ -1,5 +1,42 @@
 package com.mygdx.game;
 
+//Created by Kenneth Adair in Group 3 Assignment 2
 public class Card {
 
-}
+    //Enumerated Suit data type
+    public  enum Suit{
+        HEARTS, SPADES, CLUBS, DIAMONDS
+    }
+
+    //Enumerated Face data type
+    public enum Face{
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+
+    protected Suit mySuit;
+    protected Face myFace;
+    
+    //Constructor
+    public Card(Suit theSuit, Face theFace){
+        mySuit = theSuit;
+        myRank = theRank;
+    }
+    
+    //This method returns the suit of the card as a string.  
+    //So if the card is HEARTS when you use this method it will return "HEARTS"
+    public String returnSuit(){
+        return this.mySuit.toString();
+    }
+    
+    //This method returns the "rank" (number or face value) of the card as a string
+    //So if the card is TWO then the method will return a String that says "TWO"
+    //If the card is a King it will return a string that says "KING"
+    public String returnFace(){
+        return this.myFace.toString();
+    }
+
+    public String returnCard(){
+        return (myRank.toString() + " of " + mySuit.toString());
+    }
+
+}//End of Card
