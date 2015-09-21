03f5b3bab914c7fdc102022456fe4913a049281c
Spencer <spence521@yahoo.com>
2015-09-20 20:54:42 -0600

    Added to Dealer class

--- core/src/com/mygdx/game/Dealer.java
+++ core/src/com/mygdx/game/Dealer.java
@@ -1,22 +1,93 @@
 package com.mygdx.game;
 
+import java.util.Stack;
+
 public class Dealer {
 
 	private int dealNumber = 0;
+	private Stack<Card> firstColumn;
+	private Stack<Card> secondColumn;
+	private Stack<Card> thirdColumn;
 	
-	public Dealer(){
+	public Dealer()
+	{
+		firstColumn = new Stack<Card>();
+		secondColumn = new Stack<Card>();
+		thirdColumn = new Stack<Card>();
+	}
+	
+	public void deal(Stack<Card> deck)
+	{
+		for (int i = deck.size(); i > 0; i=i-3)
+		{
+			firstColumn.add(deck.pop());
+			secondColumn.add(deck.pop());
+			thirdColumn.add(deck.pop());
+		}
+		dealNumber++;
+	}
+	
+	public void revealCard()
+	{
 		
 	}
 	
-	public void deal(){
-		
+	public Stack<Card> pickupCards(int deckSelected)
+	{
+		Stack<Card> newDeck = new Stack<Card>();
+		if(deckSelected == 1)
+		{
+			for(int i = secondColumn.size(); i > 0; i--)
+			{
+				newDeck.add(secondColumn.pop());
+			}
+			for(int i = firstColumn.size(); i > 0; i--)
+			{
+				newDeck.add(firstColumn.pop());
+			}
+			for(int i = thirdColumn.size(); i > 0; i--)
+			{
+				newDeck.add(thirdColumn.pop());
+			}
+		}
+		else if(deckSelected == 2)
+		{
+			for(int i = firstColumn.size(); i > 0; i--)
+			{
+				newDeck.add(firstColumn.pop());
+			}
+			for(int i = secondColumn.size(); i > 0; i--)
+			{
+				newDeck.add(secondColumn.pop());
+			}
+			for(int i = thirdColumn.size(); i > 0; i--)
+			{
+				newDeck.add(thirdColumn.pop());
+			}
+		}
+		else if(deckSelected == 3)
+		{
+			for(int i = firstColumn.size(); i > 0; i--)
+			{
+				newDeck.add(firstColumn.pop());
+			}
+			for(int i = thirdColumn.size(); i > 0; i--)
+			{
+				newDeck.add(thirdColumn.pop());
+			}
+			for(int i = secondColumn.size(); i > 0; i--)
+			{
+				newDeck.add(secondColumn.pop());
+			}
+		}
+		firstColumn.clear();
+		secondColumn.clear();
+		thirdColumn.clear();
+		return newDeck;
 	}
-	
-	public void revealCard(){
-		
-	}
-	
-	public void pickupCards(){
-		
+
+	public int getDealNumber()
+	{
+		return dealNumber;
 	}
 }
